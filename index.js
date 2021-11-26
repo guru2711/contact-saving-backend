e9a003c79a14737bff833dc5fea6d90e73551025
Guru <59472229+guru2711@users.noreply.github.com>
2021-11-26 19:55:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use("/api/contacts", require("./Routes/contacts"));
 app.use("/api/auth", require("./Routes/auth"));
 
 // production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+//   );
+// }
 
 app.listen(PORT, () => {
   console.log("server is running");
